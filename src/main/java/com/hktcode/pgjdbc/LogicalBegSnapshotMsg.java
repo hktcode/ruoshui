46f90f34f10297bb26d626915807d218643d0d95
hktcode <hktcode@outlook.com>
2019-06-18 17:08:13 +0800

    add LogicalBegSnapshotMsg class

---
+++ src/main/java/com/hktcode/pgjdbc/LogicalBegSnapshotMsg.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2019, Huang Ketian
+ */
+package com.hktcode.pgjdbc;
+
+import com.google.common.collect.ImmutableList;
+import com.hktcode.lang.exception.ArgumentNullException;
+
+/**
+ * 表示开始快照的消息.
+ *
+ * 当前PostgreSQL流复制协议中并不会产生这个消息。
+ * 这个消息是本程序在获取快照时发出的。
+ */
+public class LogicalBegSnapshotMsg implements LogicalMsg
+{
+    /**
+     * 根据指定的关系列表构建{@link LogicalBegSnapshotMsg}对象.
+     *
+     * @param relalist 快照内容中所列出的关系列表.
+     *
+     * @return 包含{@code relalist}中所指定的关系的快照开始的消息.
+     * @throws ArgumentNullException if {@code relalist} is null.
+     */
+    public static LogicalBegSnapshotMsg of(ImmutableList<PgReplRelation> relalist)
+    {
+        if (relalist == null) {
+            throw new ArgumentNullException("relalist");
+        }
+        return new LogicalBegSnapshotMsg(relalist);
+    }
+
+    /**
+     * 快照内容中所列出的关系列表.
+     */
+    public final ImmutableList<PgReplRelation> relalist;
+
+    /**
+     * 构造函数.
+     *
+     * @param relalist 快照内容总所列出的关系列表.
+     */
+    private LogicalBegSnapshotMsg(ImmutableList<PgReplRelation> relalist)
+    {
+        this.relalist = relalist;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString()
+    {
+        final String format = "%12s";
+        return String.format(format, "snapshot-starts");
+    }
+}
