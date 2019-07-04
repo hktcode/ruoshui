cb869ddb24c7a70a0dfcbd62edeeb54baa6ab521
hktcode <hktcode@outlook.com>
2019-07-04 09:00:43 +0800

    add SimpleBgStatus interface

---
+++ src/main/java/com/hktcode/bgsimple/status/SimpleBgStatus.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2019, Huang Ketian.
+ */
+
+package com.hktcode.bgsimple.status;
+
+import com.hktcode.bgmethod.SimpleBgWorker;
+
+public interface SimpleBgStatus<T extends SimpleBgWorker<T>>
+{
+}
