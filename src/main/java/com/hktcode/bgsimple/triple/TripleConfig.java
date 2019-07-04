16ca8f065ac3126bedef20ea35e83a29041657e8
hktcode <hktcode@outlook.com>
2019-07-04 19:37:32 +0800

    add TrileConfig class

---
+++ src/main/java/com/hktcode/bgsimple/triple/TripleConfig.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2019, Huang Ketian.
+ */
+package com.hktcode.bgsimple.triple;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.hktcode.lang.exception.ArgumentNullException;
+
+import java.time.ZonedDateTime;
+
+public abstract class TripleConfig<M extends TripleMetric> implements TripleEntity
+{
+    public static final long DEFALUT_WAIT_TIMEOUT = 100;
+
+    public static final long DEFAULT_LOG_DURATION = 10 * 1000L;
+
+    public long waitTimeout;
+
+    public long logDuration;
+
+    public abstract M buildMutableMetric(ZonedDateTime startMillis);
+
+    // TODO: loggerLevel
+    protected TripleConfig(long waitTimeout, long logDuration)
+    {
+        this.waitTimeout = waitTimeout;
+        this.logDuration = logDuration;
+    }
+
+    @Override
+    public void toJsonObject(ObjectNode node)
+    {
+        if (node == null) {
+            throw new ArgumentNullException("node");
+        }
+        node.put("wait_timeout", this.waitTimeout);
+        node.put("log_duration", this.logDuration);
+    }
+}
