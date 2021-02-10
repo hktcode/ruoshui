807029c4df14c0220b7a3d81c3f7f6306a1cb9d3
hktcode <hktcode@outlook.com>
2021-02-10 21:31:40 +0800

    为SimpleMethodResult的run方法添加了日志。

--- src/main/java/com/hktcode/bgsimple/method/SimpleMethodResult.java
+++ src/main/java/com/hktcode/bgsimple/method/SimpleMethodResult.java
@@ -5,16 +5,20 @@ package com.hktcode.bgsimple.method;
 
 import com.hktcode.bgsimple.BgWorker;
 import com.hktcode.lang.exception.ArgumentNullException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public interface SimpleMethodResult extends SimpleMethod
 {
+    Logger logger = LoggerFactory.getLogger(SimpleMethodResult.class);
+
     @Override
     default SimpleMethodResult run(BgWorker wkstep)
     {
         if (wkstep == null) {
             throw new ArgumentNullException("wkstep");
         }
-        // TODO: logger this should not be happened
+        logger.error("this should not be happened.");
         return this;
     }
 }
