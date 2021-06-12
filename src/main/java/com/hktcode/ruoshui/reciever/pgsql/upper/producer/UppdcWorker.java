e89640f7838f005039c30b508f48340d5da36952
hktcode <hktcode@outlook.com>
2021-06-12 14:31:25 +0800

    重构UppdcWorker：根据新的SimpleWorker重写，为将来废弃UppdcWorker做准备

--- src/main/java/com/hktcode/ruoshui/reciever/pgsql/upper/producer/UppdcWorker.java
+++ src/main/java/com/hktcode/ruoshui/reciever/pgsql/upper/producer/UppdcWorker.java
@@ -1,14 +1,12 @@
 package com.hktcode.ruoshui.reciever.pgsql.upper.producer;
 
 import com.hktcode.lang.exception.ArgumentNullException;
-import com.hktcode.queue.Tqueue;
-import com.hktcode.ruoshui.reciever.pgsql.upper.UpperRecordProducer;
 import com.hktcode.simple.SimpleAtomic;
 import com.hktcode.simple.SimpleWorker;
 
 public class UppdcWorker extends SimpleWorker<UppdcWorkerArgval, UppdcWorkerMeters>
 {
-    public static UppdcWorker of(UppdcWorkerArgval argval, UppdcWorkerMeters meters, SimpleAtomic holder, Tqueue<UpperRecordProducer> target)
+    public static UppdcWorker of(UppdcWorkerArgval argval, UppdcWorkerMeters meters, SimpleAtomic holder)
     {
         if (argval == null) {
             throw new ArgumentNullException("argval");
@@ -19,23 +17,11 @@ public class UppdcWorker extends SimpleWorker<UppdcWorkerArgval, UppdcWorkerMete
         if (holder == null) {
             throw new ArgumentNullException("holder");
         }
-        if (target == null) {
-            throw new ArgumentNullException("target");
-        }
-        return new UppdcWorker(argval, meters, holder, target);
+        return new UppdcWorker(argval, meters, holder);
     }
 
-    private final Tqueue<UpperRecordProducer> target;
-
-    private UppdcWorker(UppdcWorkerArgval argval, UppdcWorkerMeters meters, SimpleAtomic holder, Tqueue<UpperRecordProducer> target)
+    private UppdcWorker(UppdcWorkerArgval argval, UppdcWorkerMeters meters, SimpleAtomic holder)
     {
         super(argval, meters, holder);
-        this.target = target;
-    }
-
-    @Override
-    public UppdcWkstepAction action()
-    {
-        return this.argval.actionInfos.get(0).action(this.target);
     }
 }
