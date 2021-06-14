a0453aefcc2d44bb939e0f8b6ba4775c85bf1dba
hktcode <hktcode@outlook.com>
2021-06-14 11:33:35 +0800

    整理SimpleWkstepAction：重命名holder为atomic

--- src/main/java/com/hktcode/simple/SimpleWkstepAction.java
+++ src/main/java/com/hktcode/simple/SimpleWkstepAction.java
@@ -7,5 +7,5 @@ package com.hktcode.simple;
 public interface SimpleWkstepAction<A extends SimpleWorkerArgval<A, G>, G extends SimpleWorkerGauges>
         extends SimpleWkstep
 {
-    SimpleWkstep next(A argval, G gauges, SimpleAtomic holder) throws Throwable;
+    SimpleWkstep next(A argval, G gauges, SimpleAtomic atomic) throws Throwable;
 }
