25edde742d9ac7f8891d0feb60b98635d78399d2
hktcode <hktcode@outlook.com>
2021-06-20 15:50:04 +0800

    重构Upcsm：重命名UpcsmWkstep为UpcsmRecver

--- src/main/java/com/hktcode/ruoshui/reciever/pgsql/upper/consumer/UpcsmRecverArgval.java
+++ src/main/java/com/hktcode/ruoshui/reciever/pgsql/upper/consumer/UpcsmRecverArgval.java
@@ -1,7 +1,3 @@
-/*
- * Copyright (c) 2019, Huang Ketian.
- */
-
 package com.hktcode.ruoshui.reciever.pgsql.upper.consumer;
 
 import com.fasterxml.jackson.databind.JsonNode;
@@ -11,29 +7,30 @@ import com.hktcode.lang.exception.ArgumentNullException;
 import com.hktcode.ruoshui.reciever.pgsql.entity.LogicalReplArgval;
 import com.hktcode.ruoshui.reciever.pgsql.entity.PgConnectionProperty;
 
-public class UpcsmWkstepArgval
+public class UpcsmRecverArgval
 {
-    public final static ObjectNode SCHEMA = JacksonObject.getFromResource(UpcsmWkstepArgval.class, "UpcsmArgval.yml");
+    public final static ObjectNode SCHEMA = JacksonObject.getFromResource(UpcsmRecverArgval.class, "UpcsmRecver.yml");
 
-    public static UpcsmWkstepArgval of(JsonNode json) //
+    public static UpcsmRecverArgval of(JsonNode json) //
     {
         if (json == null) {
             throw new ArgumentNullException("json");
         }
+        // - JsonNode srcPropertyNode = json.path("sender_class");
         JsonNode srcPropertyNode = json.path("src_property");
         PgConnectionProperty srcProperty = PgConnectionProperty.ofJsonObject(srcPropertyNode);
 
         JsonNode logicalReplNode = json.path("logical_repl");
         LogicalReplArgval logicalRepl = LogicalReplArgval.of(logicalReplNode);
 
-        return new UpcsmWkstepArgval(srcProperty, logicalRepl);
+        return new UpcsmRecverArgval(srcProperty, logicalRepl);
     }
 
     public final PgConnectionProperty srcProperty;
 
     public final LogicalReplArgval logicalRepl;
 
-    protected UpcsmWkstepArgval(PgConnectionProperty srcProperty, LogicalReplArgval logicalRepl)
+    private UpcsmRecverArgval(PgConnectionProperty srcProperty, LogicalReplArgval logicalRepl)
     {
         this.srcProperty = srcProperty;
         this.logicalRepl = logicalRepl;
@@ -45,15 +42,11 @@ public class UpcsmWkstepArgval
     // -     if (node == null) {
     // -         throw new ArgumentNullException("node");
     // -     }
+    // -     node = super.toJsonObject(node);
     // -     ObjectNode logicalReplNode = node.putObject("logical_repl");
     // -     this.logicalRepl.toJsonObject(logicalReplNode);
     // -     ObjectNode srcPropertyNode = node.putObject("src_property");
     // -     this.srcProperty.toJsonObject(srcPropertyNode);
     // -     return node;
-    // - }
-
-    // - public UpcsmWkstepAction action()
-    // - {
-    // -     return UpcsmWkstepAction.of();
     // - }
 }
