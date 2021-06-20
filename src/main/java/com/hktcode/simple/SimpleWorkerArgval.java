dece3c92c39120b128b82c66943678ced5deb627
hktcode <hktcode@outlook.com>
2021-06-20 09:55:54 +0800

    整理SimpleWorkerGauges：添加了LOG_DURATION配置

--- src/main/java/com/hktcode/simple/SimpleWorkerArgval.java
+++ src/main/java/com/hktcode/simple/SimpleWorkerArgval.java
@@ -5,5 +5,7 @@ import com.hktcode.jackson.JacksonObject;
 public interface SimpleWorkerArgval<A extends SimpleWorkerArgval<A, G>, G extends SimpleWorkerGauges>
         extends JacksonObject
 {
+    long LOG_DURATION = 5 * 60 * 1000;
+
     SimpleWkstepAction<A, G> action();
 }
