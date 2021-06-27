88d1eee4c45dc31b9ae60747b2e157b28bff8d06
hktcode <hktcode@outlook.com>
2021-06-27 17:45:06 +0800

    重构UppdcWorker：合并了UppdcWorkerArgval和UppdcWkstepAction。

--- src/main/java/com/hktcode/ruoshui/reciever/pgsql/upper/producer/UppdcWorkerArgval.java
+++ src/main/java/com/hktcode/ruoshui/reciever/pgsql/upper/producer/UppdcWorkerArgval.java
@@ -7,13 +7,17 @@ import com.hktcode.jackson.JacksonObject;
 import com.hktcode.lang.exception.ArgumentNullException;
 import com.hktcode.queue.Xqueue;
 import com.hktcode.ruoshui.reciever.pgsql.upper.UpperRecordProducer;
-import com.hktcode.simple.SimpleWorkerArgval;
-import com.hktcode.simple.SimpleWorkerGauges;
+import com.hktcode.simple.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class UppdcWorkerArgval extends SimpleWorkerGauges //
         implements SimpleWorkerArgval<UppdcWorkerArgval, UppdcWorkerArgval>
+                 , SimpleWkstepAction<UppdcWorkerArgval, UppdcWorkerArgval>
 {
     public static final ObjectNode SCHEMA;
 
@@ -76,8 +80,48 @@ public class UppdcWorkerArgval extends SimpleWorkerGauges //
     }
 
     @Override
-    public UppdcWkstepAction action()
+    public UppdcWorkerArgval action()
     {
-        return UppdcWkstepAction.of();
+        return this;
     }
+
+
+    @Override
+    public SimpleWkstep next(UppdcWorkerArgval argval, UppdcWorkerArgval gauges, SimpleAtomic atomic) ///
+            throws Throwable
+    {
+        if (argval == null) {
+            throw new ArgumentNullException("argval");
+        }
+        if (gauges == null) {
+            throw new ArgumentNullException("gauges");
+        }
+        if (atomic == null) {
+            throw new ArgumentNullException("atomic");
+        }
+        List<UpperRecordProducer> lhs, rhs = argval.recver.list();
+        long now, prelog = System.currentTimeMillis(), spins = 0;
+        Iterator<UpperRecordProducer> iter = rhs.iterator();
+        Xqueue.Fetch<UpperRecordProducer> recver = argval.recver.fetchXqueue();
+        try (UppdcSender.Client client = argval.sender.client()) {
+            while (atomic.call(Long.MAX_VALUE).deletets == Long.MAX_VALUE) {
+                long l = argval.xspins.logDuration;
+                if (iter.hasNext()) {
+                    // 未来计划：send方法支持数组，发送多个记录，提高性能
+                    client.send(iter.next());
+                } else if ((lhs = recver.poll(rhs)) != rhs) {
+                    rhs = lhs;
+                    iter = rhs.iterator();
+                } else if (prelog + l >= (now = System.currentTimeMillis())) {
+                    logger.info("write to logDuration={}", l);
+                    prelog = now;
+                } else {
+                    gauges.xspins.spins(spins++);
+                }
+            }
+        }
+        return SimpleWkstepTheEnd.of();
+    }
+
+    private static final Logger logger = LoggerFactory.getLogger(UppdcWorkerArgval.class);
 }
