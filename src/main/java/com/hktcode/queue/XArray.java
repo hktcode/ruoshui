0773f9aed94cdf11e8df6ff00039fce2ca9a08ab
hktcode <hktcode@outlook.com>
2021-07-10 16:53:05 +0800

    重构Xqueue：拆分了XArray和Xqueue。

---
+++ src/main/java/com/hktcode/queue/XArray.java
@@ -0,0 +1,87 @@
+package com.hktcode.queue;
+
+import java.util.Iterator;
+
+public class XArray<E>
+{
+    public static <E> XArray<E> of(int maxCapacity)
+    {
+        return new XArray<>(maxCapacity);
+    }
+
+    private Object[] list; // 修改为private
+
+    private int size = 0; // 修改为private，并提供size()方法
+
+    private XArray(int maxCapacity)
+    {
+        this.list = new Object[maxCapacity];
+    }
+
+    public boolean add(E item)
+    {
+        if (size >= this.list.length) {
+            return false;
+        }
+        this.list[this.size++] = item;
+        return true;
+    }
+
+    public void clear()
+    {
+        // 赋值为null，减少内存泄露。
+        while (size > 0) {
+            this.list[--size] = null;
+        }
+    }
+
+    public Iterator<E> iterator()
+    {
+        return new XIterator<>(this);
+    }
+
+    public int getCapacity()
+    {
+        return this.list.length;
+    }
+
+    public int getSize()
+    {
+        return this.size;
+    }
+
+    public void setCapacity(int maxCapacity)
+    {
+        if (maxCapacity < this.size) {
+            return;
+        }
+        Object[] l = new Object[maxCapacity];
+        System.arraycopy(this.list, 0, l, 0, this.size);
+        this.list = l;
+    }
+
+    public static class XIterator<E> implements Iterator<E>
+    {
+        private XIterator(XArray<E> xarray)
+        {
+            this.xarray = xarray;
+        }
+
+        private final XArray<E> xarray;
+
+        private int index = 0;
+
+        @Override
+        public boolean hasNext()
+        {
+            return index < xarray.size;
+        }
+
+        @Override
+        @SuppressWarnings("unchecked")
+        public E next()
+        {
+            return (E)xarray.list[index++];
+        }
+    }
+}
--- src/main/java/com/hktcode/queue/Xqueue.java
+++ src/main/java/com/hktcode/queue/Xqueue.java
@@ -1,138 +1,236 @@
 package com.hktcode.queue;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.hktcode.jackson.JacksonObject;
 import com.hktcode.lang.exception.ArgumentNullException;
+import com.hktcode.ruoshui.reciever.pgsql.upper.JsonResult;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class Xqueue<E>
 {
-    public static <E> Xqueue<E> of(JsonNode json)
+    public static class Schema
     {
-        if (json == null) {
-            throw new ArgumentNullException("json");
+        public static final ObjectNode SCHEMA;
+
+        // - $schema: http://json-schema.org/draft-04/schema#
+        // - type: object
+        // - properties:
+        // -   max_messages: { type: integer, default: 1024, minimum: 0, maximum: 2147483647 }
+        // -   min_messages: { type: integer, default: 1024, minimum: 0, maximum: 2147483647 }
+        // -   max_duration: { type: integer, default: 1024, minimum: 0, maximum: 2147483647 }
+        static
+        {
+            ObjectNode schema = new ObjectNode(JsonNodeFactory.instance);
+            schema.put("$schema", "http://json-schema.org/draft-04/schema#");
+            schema.put("type", "object");
+            ObjectNode argvalNode = schema.putObject("properties");
+            putInteger(argvalNode, "max_messages", DEFAULT_MAX_MESSAGES, 1, Integer.MAX_VALUE);
+            putInteger(argvalNode, "min_messages", DEFAULT_MIN_MESSAGES, 1, Integer.MAX_VALUE);
+            putInteger(argvalNode, "max_duration", DEFAULT_MAX_DURATION, 0, Long.MAX_VALUE);
+            SCHEMA = JacksonObject.immutableCopy(schema);
         }
-        return new Xqueue<>(json.asInt(MAX_CAPACITY));
+
+        private static void putInteger(ObjectNode node, String name, long defval, long minimum, long maximum)
+        {
+            ObjectNode result = node.putObject(name);
+            result.put("type", "integer");
+            result.put("default", defval);
+            result.put("minimum", minimum);
+            result.put("maximum", maximum);
+        }
     }
 
-    public static final int MAX_CAPACITY = 1024;
+    public static <E> Xqueue<E> of(JsonNode node)
+    {
+        if (node == null) {
+            throw new ArgumentNullException("node");
+        }
+        Xqueue<E> result = new Xqueue<>();
+        result.pst(node);
+        return result;
+    }
 
-    private final AtomicReference<List<E>> atomic; // TODO: 采用数组，而不是List，自定义此对象
+    public static final int DEFAULT_MAX_MESSAGES = 1024;
 
-    public int maxCapacity;
+    public static final int DEFAULT_MIN_MESSAGES = 16;
+
+    public static final int DEFAULT_MAX_DURATION = 8;
+
+    private Xqueue()
+    {
+        this.atomicInner = new AtomicReference<>(XArray.of(0));
+    }
 
     // - argval
-    // -
-    // - public long minMessages;
-    // - public long maxMessages;
+
+    private int maxMessages = DEFAULT_MAX_MESSAGES;
+
+    private int minMessages = DEFAULT_MIN_MESSAGES;
+
+    private long maxDuration = DEFAULT_MAX_DURATION;
 
     // - gauges
-    // -
-    // - public long fetchTrycnt;
-    // - public long fetchSuccnt;
-    // - public long fetchCounts;
 
-    // - public long offerTrycnt;
-    // - public long offerSuccnt;
-    // - public long offerCounts;
+    private final AtomicReference<XArray<E>> atomicInner;
 
-    public static class XList<E>
+    private long offerMillis = 0;
+
+    private long offerTrycnt = 0;
+
+    private long offerRowcnt = 0;
+
+    private long offerCounts = 0;
+
+    private long fetchTrycnt = 0;
+
+    private long fetchRowcnt = 0;
+
+    private long fetchCounts = 0;
+
+    public XArray<E> push(XArray<E> lhs)
     {
-        public final E[] list;
-
-        public int size = 0;
-
-        private XList(E[] list)
-        {
-            this.list = list;
+        if (lhs == null) {
+            throw new ArgumentNullException("lhs");
         }
-    }
-
-    private Xqueue(int maxCapacity)
-    {
-        this.maxCapacity = maxCapacity;
-        this.atomic = new AtomicReference<>(new ArrayList<>(maxCapacity));
-    }
-
-    public Offer<E> offerXqueue()
-    {
-        return new Offer<>(this);
-    }
-
-    public Fetch<E> fetchXqueue()
-    {
-        return new Fetch<>(this);
-    }
-
-    public List<E> list()
-    {
-        return new ArrayList<>(this.maxCapacity);
-    }
-
-    public static class Offer<E>
-    {
-        private final Xqueue<E> xqueue;
-
-        public long trycnt = 0;
-        public long bufcnt = 0;
-        public long rowcnt = 0;
-
-        private Offer(Xqueue<E> xqueue)
-        {
-            this.xqueue = xqueue;
-        }
-
-        public List<E> push(List<E> lhs)
-        {
-            if (lhs == null) {
-                throw new ArgumentNullException("lhs");
-            }
-            ++this.trycnt;
-            List<E> rhs = this.xqueue.atomic.get();
-            if (!rhs.isEmpty()) {
-                return lhs;
-            }
-            this.rowcnt += lhs.size();
-            this.xqueue.atomic.set(lhs);
-            ++this.bufcnt;
-            return rhs;
-        }
-    }
-
-    public static class Fetch<E>
-    {
-        private final Xqueue<E> xqueue;
-
-        public long trycnt = 0;
-        public long bufcnt = 0;
-        public long rowcnt = 0;
-
-        private Fetch(Xqueue<E> xqueue)
-        {
-            this.xqueue = xqueue;
-        }
-
-        public List<E> poll(List<E> rhs)
-        {
-            if (rhs == null) {
-                throw new ArgumentNullException("rhs");
-            }
-            rhs.clear();
-            ++this.trycnt;
-            List<E> lhs = this.xqueue.atomic.get();
-            if (lhs.isEmpty()) {
-                return rhs;
-            }
-            this.rowcnt += lhs.size();
-            this.xqueue.atomic.set(rhs);
-            ++this.bufcnt;
+        ++this.offerTrycnt;
+        XArray<E> rhs = this.atomicInner.get();
+        // 未来计划：支持bufferCount和maxDuration
+        if (rhs.getSize() != 0) {
             return lhs;
         }
+        this.offerRowcnt += lhs.getSize();
+        this.atomicInner.set(lhs);
+        ++this.offerCounts;
+        rhs.setCapacity(this.maxMessages);
+        return rhs;
+    }
 
-        // - public String toStringText();
-        // - public String getConfigObj();
-        // - public String getMetricObj();
+    public XArray<E> poll(XArray<E> rhs)
+    {
+        if (rhs == null) {
+            throw new ArgumentNullException("rhs");
+        }
+        rhs.clear();
+        ++this.fetchTrycnt;
+        XArray<E> lhs = this.atomicInner.get();
+        int size = lhs.getSize();
+        if (size == 0) {
+            return rhs;
+        }
+        this.fetchRowcnt += size;
+        ++this.fetchCounts;
+        this.atomicInner.set(rhs);
+        return lhs;
+    }
+
+    public void pst(JsonNode node)
+    {
+        if (node == null) {
+            throw new ArgumentNullException("node");
+        }
+        this.maxMessages = node.path("max_messages").asInt(this.maxMessages);
+        this.minMessages = node.path("min_messages").asInt(this.minMessages);
+        this.maxDuration = node.path("max_duration").asLong(this.maxDuration);
+        this.atomicInner.get().setCapacity(this.maxMessages);
+    }
+
+    public XArray<E> newArray()
+    {
+        return XArray.of(this.maxMessages);
+    }
+
+    public Result toJsonResult()
+    {
+        return new Result(new Config(this), new Metric(this));
+    }
+
+    public static class Result extends JsonResult<Config, Metric>
+    {
+        private Result(Config config, Metric metric)
+        {
+            super(config, metric);
+        }
+    }
+
+    public static class Config implements JacksonObject
+    {
+        public final int maxMessages;
+
+        public final int minMessages;
+
+        public final long maxDuration;
+
+        // - public final long minDuration;
+
+        private <E> Config(Xqueue<E> sender)
+        {
+            this.maxMessages = sender.maxMessages;
+            this.minMessages = sender.minMessages;
+            this.maxDuration = sender.maxDuration;
+        }
+
+        @Override
+        public ObjectNode toJsonObject(ObjectNode node)
+        {
+            if (node == null) {
+                throw new ArgumentNullException("node");
+            }
+            node.put("max_messages", this.maxMessages);
+            node.put("min_messages", this.minMessages);
+            node.put("max_duration", this.maxDuration);
+            return node;
+        }
+    }
+
+    public static class Metric implements JacksonObject
+    {
+        public final long fetchTrycnt;
+
+        public final long fetchRowcnt;
+
+        public final long fetchCounts;
+
+        public final long offerTrycnt;
+
+        public final long offerRowcnt;
+
+        public final long offerCounts;
+
+        public final long curMessages;
+
+        public final long curCapacity;
+
+        private <E> Metric(Xqueue<E> sender)
+        {
+            this.fetchTrycnt = sender.fetchTrycnt;
+            this.fetchRowcnt = sender.fetchRowcnt;
+            this.fetchCounts = sender.fetchCounts;
+            this.offerTrycnt = sender.offerTrycnt;
+            this.offerRowcnt = sender.offerRowcnt;
+            this.offerCounts = sender.offerCounts;
+            XArray<E> list = sender.atomicInner.get();
+            this.curCapacity = list.getCapacity();
+            this.curMessages = list.getSize();
+        }
+
+        @Override
+        public ObjectNode toJsonObject(ObjectNode node)
+        {
+            if (node == null) {
+                throw new ArgumentNullException("node");
+            }
+            node.put("offer_trycnt", this.offerTrycnt);
+            node.put("offer_rowcnt", this.offerRowcnt);
+            node.put("offer_counts", this.offerCounts);
+            node.put("fetch_trycnt", this.fetchTrycnt);
+            node.put("fetch_rowcnt", this.fetchRowcnt);
+            node.put("fetch_counts", this.fetchCounts);
+            node.put("cur_capacity", this.curCapacity);
+            node.put("cur_messages", this.curMessages);
+            return node;
+        }
     }
 }
